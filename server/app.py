589fde254a1d6dfa58eae62de677b4a906873362
edwin <edmboche141@gmail.com>
2025-03-31 10:42:00 +0300

    added some comment

--- server/app.py
+++ server/app.py
@@ -1,23 +1,27 @@
+# importing modules from the project directory
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
 from flask import Flask, request, make_response, jsonify
 import os
 
+# difining the base directory and the data base url
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
+app = Flask(__name__) # a flask application instance
 
-migrate = Migrate(app, db)
-db.init_app(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE # cofiguring the database 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # THIS disables the sqlalchemy modification tracking
+app.json.compact = False # this sets the json output be more compact
 
-@app.route('/restaurants', methods=['GET'])
+migrate = Migrate(app, db) # this initializes flask migrate extention
+db.init_app(app) # initializes sqlalchemy with the flask application
+
+# aplication routes 
+@app.route('/restaurants', methods=['GET']) # route with the method 
 def get_restaurants():
     restaurants = Restaurant.query.all()
-    return jsonify([restaurant.to_dict(only=('id', 'name', 'address')) for restaurant in restaurants]), 200
+    return jsonify([restaurant.to_dict(only=('id', 'name', 'address')) for restaurant in restaurants]), 200 #converts each restaurant object to a dict and return a json
 
 @app.route('/restaurants/<int:id>', methods=['GET'])
 def get_restaurant_by_id(id):
@@ -25,7 +29,6 @@ def get_restaurant_by_id(id):
     if not restaurant:
         return jsonify({'error': 'Restaurant not found'}), 404
 
-    # Include nested restaurant_pizzas and their pizzas
     restaurant_data = {
         'id': restaurant.id,
         'name': restaurant.name,
@@ -46,6 +49,8 @@ def get_restaurant_by_id(id):
 
     return jsonify(restaurant_data), 200
 
+
+# route for deleting restaurant with id
 @app.route('/restaurants/<int:id>', methods=['DELETE'])
 def delete_restaurant(id):
     restaurant = Restaurant.query.filter_by(id=id).first()
@@ -55,25 +60,28 @@ def delete_restaurant(id):
     db.session.commit()
     return make_response('', 204)
 
+# route for geting all pizzas
 @app.route('/pizzas', methods=['GET'])
 def get_pizzas():
     pizzas = Pizza.query.all()
     return jsonify([pizza.to_dict(only=('id', 'name', 'ingredients')) for pizza in pizzas]), 200
 
+
+# route for creating new restaurant_pizza
 @app.route('/restaurant_pizzas', methods=['POST'])
 def create_restaurant_pizza():
     data = request.get_json()
 
-    try:
+    try: #create new restaurant with the data
         new_restaurant_pizza = RestaurantPizza(
             price=data['price'],
             restaurant_id=data['restaurant_id'],
             pizza_id=data['pizza_id']
-        )
+        ) # commiting to a database
         db.session.add(new_restaurant_pizza)
         db.session.commit()
     except ValueError:
-        # Update the error message to match the test case
+
         return jsonify({"errors": ["validation errors"]}), 400
 
     restaurant_pizza_data = {
@@ -95,5 +103,6 @@ def create_restaurant_pizza():
 
     return jsonify(restaurant_pizza_data), 201
 
+# running the flask app
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
